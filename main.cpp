f4e2c8ac56d48cd168bebb3d6921ec134d292a2a
Daniel Brenes <danbg31@gmail.com>
2020-03-14 18:20:25 -0600

    travis commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "calculadora.cpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Calculadora calculadora;
+
+    std::cout << calculadora.Multiplicar(1,3) << std::endl;
+
     return 0;
 }
