004b9a980ccd0cf9a4a5f04a743178634626ed80
Daniel Brenes <danbg31@gmail.com>
2020-03-14 18:32:43 -0600

    sonar commit

---
+++ cmake-build-debug/CMakeFiles/sonar_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sonar_cpp.dir/main.cpp.o"
+  "sonar_cpp"
+  "sonar_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sonar_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
